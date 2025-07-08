1710926c62ef799d50c37e1d025902df31a47284
Mariano Curci <curci.mariano2003@gmail.com>
2025-07-08 12:40:23 +0200

    first commit

---
+++ src/main/java/it/aulab/progetto_finale_curci_mariano/ProgettoFinaleCurciMarianoApplication.java
@@ -0,0 +1,13 @@
+package it.aulab.progetto_finale_curci_mariano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoFinaleCurciMarianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoFinaleCurciMarianoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/aulab/progetto_finale_curci_mariano/ProgettoFinaleCurciMarianoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.aulab.progetto_finale_curci_mariano;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoFinaleCurciMarianoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
